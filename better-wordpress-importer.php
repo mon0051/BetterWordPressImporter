0f1df9a6737b45af779d1b14f82e7ca967061b9c
Andrew Monteith <andrew.monteith1@gmail.com>
2014-12-15 11:10:42 +1100

    Version Bump

--- better-wordpress-importer.php
+++ better-wordpress-importer.php
@@ -3,7 +3,7 @@
 Plugin Name: Better Wordpress Importer
 Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
-Version: 1.0
+Version: 1.0a
 Author: mon
 Author URI: http://URI_Of_The_Plugin_Author
 License: GPL2
